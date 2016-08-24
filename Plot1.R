ebb66128bf6206164a9a055c871ab6ccbdadaded
sutapalli <eswara-raos@hpe.com>
2016-08-24 15:40:15 +0530

    Exploratory Data Project-1

---
+++ Plot1.R
@@ -0,0 +1,11 @@
+MakePlot1 <- function() {  
+    SourceDataFile <- "household_power_consumption.txt" 
+    ReadData <- read.table(SourceDataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".") 
+    subSetData <- ReadData[ReadData$Date %in% c("1/2/2007","2/2/2007") ,] 
+    globalActivePower <- as.numeric(subSetData$Global_active_power) 
+    png("plot1.png", width=480, height=480) 
+    hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)") 
+    dev.off() 
+}
+
+MakePlot1()
---
+++ Plot2.R
@@ -0,0 +1,13 @@
+GetDataAndMakePlot <- function() 
+  {
+    SourceDataFile <- "household_power_consumption.txt" 
+    Data <- read.table(SourceDataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".") 
+    subSetData <- Data[Data$Date %in% c("1/2/2007","2/2/2007") ,] 
+    datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S")  
+    globalActivePower <- as.numeric(subSetData$Global_active_power) 
+    png("plot2.png", width=480, height=480) 
+    plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)") 
+    dev.off()
+  }
+
+GetDataAndMakePlot()
